44f6465ad788a0b61d355556dfc450932c535953
andreeaCcss <andreeaccss@gmail.com>
2019-09-13 11:38:29 +0200

    send message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import request from "superagent";
+// connect to stream
+// socket.io for server driven comm or
+// web sockets - dff protocol - doesnt use http
+// server sent events - built into JS in the browser
+// json sse
+class App extends React.Component {
+  state = {
+    text: ""
+  };
+  // evSource is built in brwoser
+  source = new EventSource("http://localhost:4000/stream");
+  componentDidMount() {
+    // when we did stream.send was a new event
+    // when a new message comes in we run tthis
+    this.source.onmessage = event => {
+      const { data } = event;
+      console.log("data:", data);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      const messages = JSON.parse(data);
+      console.log("mess", messages);
+    };
+  }
+  onChange = event => {
+    const {
+      target: { value }
+    } = event;
+
+    console.log("val", value);
+
+    this.setState({ text: value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    const { text } = this.state;
+    request
+      .post("http://localhost:4000/message")
+      .send({ text })
+      .then(console.log)
+      .catch(console.error);
+  };
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input type="text" onChange={this.onChange} value={this.state.text} />
+        <button type="button">Send</button>
+      </form>
+    );
+  }
 }
-
 export default App;
